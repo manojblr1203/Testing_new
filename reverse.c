2e480996d2c4c3cf72282d8245aead3bbd933bcb
san <san@gmail.com>
2021-04-01 16:01:53 +0000

    modified reverse.c program

--- reverse.c
+++ reverse.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 void rev()
 {
   char str[1000], rev[1000];
   int i, j, count = 0;
   printf ("\n enter string to reverse:");
   scanf("%s", str);
-  printf("\nString Before Reverse: %s", str);
+  printf("\nString Before Reverse: %s\n", str);
   //finding the length of the string
   while (str[count] != '\0')
   {
@@ -22,3 +21,4 @@ void rev()
 
   printf("\nString After Reverse: %s\n", rev);
 }
+
